3a941b83154a8d86e293658412ab89905eacba37
Felipe Spirandelli <70784142+FelipeSpirandelli@users.noreply.github.com>
2021-07-16 16:07:58 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(){
+
+  return(
+    <Text>
+      Oi
+    </Text>
+
+  )
+} 
+
+export default App
