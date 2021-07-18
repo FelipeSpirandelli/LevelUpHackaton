f082eee973e12b8375870cc2897c27c882f12c47
Felipe Spirandelli <70784142+FelipeSpirandelli@users.noreply.github.com>
2021-07-17 21:10:35 -0300

    feat: add dashboard

---
+++ components/graph/graph.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+function Column({ heightValue, week }) {
+
+    return (
+        <View style={styles.columnContainer}>
+            <View style={styles.outerColumn}>
+                <View style={[styles.innerColumn, { height: `${heightValue}` }]}>
+                </View>
+            </View>
+            <Text style={styles.weekText}>
+                Week {week}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    columnContainer:{
+        width: 50,
+        display:'flex',
+        flexDirection: 'column',
+        alignItems:'center'
+    },
+    outerColumn: {
+        backgroundColor: '#FACA78',
+        height: 120,
+        width: 12,
+        borderRadius: 6,
+        margin: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    innerColumn: {
+        backgroundColor: '#F8B440',
+        position:'absolute',
+        width: 12,
+        borderRadius: 6,
+        bottom:0
+    },
+    weekText:{
+        fontSize: 12,
+    }
+
+})
+
+export default Column
